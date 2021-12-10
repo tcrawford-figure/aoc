e93220284d903a85d7ca2b9a88b4a170d93fd8b0
Tyler Crawford <tcrawford@figure.com>
2021-12-10 10:30:23 -0500

    Day 6 solution

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -1,13 +1,31 @@
 package days
 
+import java.math.BigInteger
+
 class Day6 : Day(6) {
 
-    override fun partOne(): Any {
-        return -1
+    override fun partOne(): BigInteger = simulateFish(80)
+
+    override fun partTwo(): BigInteger = simulateFish(256)
+
+    private fun simulateFish(days: Int): BigInteger {
+        val fishPerDay = parseInput()
+        repeat(days) {
+            fishPerDay.rotateLeftInPlace()
+            fishPerDay[6] += fishPerDay[8]
+        }
+        return fishPerDay.sumOf { it }
     }
 
-    override fun partTwo(): Any {
-        return -1
+    private fun <T> MutableList<T>.rotateLeftInPlace() {
+        val leftMost = first()
+        (1 until size).map { i -> this[i - 1] = this[i] }
+        this[this.lastIndex] = leftMost
     }
+
+    private fun parseInput(): MutableList<BigInteger> =
+        MutableList<BigInteger>(9) { BigInteger.ZERO }.apply {
+            inputString.split(",").map { it.toInt() }.forEach { this[it] += BigInteger.ONE }
+        }
 
 }
--- src/test/kotlin/days/Day6Test.kt
+++ src/test/kotlin/days/Day6Test.kt
@@ -3,6 +3,7 @@ package days
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 class Day6Test {
 
@@ -10,12 +11,12 @@ class Day6Test {
 
     @Test
     fun testPartOne() {
-        assertThat(day.partOne(), `is`(5934))
+        assertThat(day.partOne(), `is`(BigInteger.valueOf(5934)))
     }
 
     @Test
     fun testPartTwo() {
-        assertThat(day.partTwo(), `is`(-1))
+        assertThat(day.partTwo(), `is`(BigInteger.valueOf(26984457539)))
     }
 
 }
